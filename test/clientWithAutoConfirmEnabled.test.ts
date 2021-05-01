8e554f72b71fefeb642c320ba106f1bdd3741772
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-05-01 20:53:48 +0530

    fix test

--- test/clientWithAutoConfirmEnabled.test.ts
+++ test/clientWithAutoConfirmEnabled.test.ts
@@ -67,9 +67,9 @@ test('setSession should return no error', async () => {
   })
   expect(error).toBeNull()
   expect(session).not.toBeNull()
-  await authWithSession.setSession(session.refresh_token)
+  await authWithSession.setSession(session!.refresh_token)
   const { user } = await authWithSession.update({ data: { hello: 'hello world' } })
-  expect(user.user_metadata).toBe({ hello: 'hello world' })
+  expect(user!.user_metadata).toBe({ hello: 'hello world' })
 })
 
 test('signUp() the same user twice should throw an error', async () => {
